72014cc059a55d14cc9fbd5ba82387b5c629edfc
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-05-31 11:49:26 +0400

    Setup

---
+++ CRONMASTER/src/main/java/com/example/CRONMASTER/CronmasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRONMASTER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronmasterApplication.class, args);
+	}
+
+}
---
+++ CRONMASTER/src/test/java/com/example/CRONMASTER/CronmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRONMASTER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
