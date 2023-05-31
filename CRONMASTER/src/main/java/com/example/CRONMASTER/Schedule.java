90673f153da1058d6ed263c79129b534df2f1d74
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-05-31 14:46:18 +0400

    Q16 Update

--- CRONMASTER/src/main/java/com/example/CRONMASTER/Schedule.java
+++ CRONMASTER/src/main/java/com/example/CRONMASTER/Schedule.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Schedule {
 
-    @Scheduled(cron = "*/10 9-17 * * 1-5")
+    @Scheduled(cron = "0 * 1-15 * *")
 
     public void SchedulerTask() {
         System.out.println("Hello Codeline");
