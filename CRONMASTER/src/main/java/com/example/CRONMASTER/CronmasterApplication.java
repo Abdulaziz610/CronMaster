afbd32521301801df5fa12291cb8ee8ebc7ef1c0
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-05-31 12:02:55 +0400

    Setup2

--- CRONMASTER/src/main/java/com/example/CRONMASTER/CronmasterApplication.java
+++ CRONMASTER/src/main/java/com/example/CRONMASTER/CronmasterApplication.java
@@ -2,8 +2,10 @@ package com.example.CRONMASTER;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CronmasterApplication {
 
 	public static void main(String[] args) {
---
+++ CRONMASTER/src/main/java/com/example/CRONMASTER/Schedule.java
@@ -0,0 +1,14 @@
+package com.example.CRONMASTER;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schedule {
+
+    @Scheduled(cron = "*/5 * * * *")
+    public void SchedulerTask() {
+        System.out.println("Hello Codeline");
+    }
+}
